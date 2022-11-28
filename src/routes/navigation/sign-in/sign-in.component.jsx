9a9f38f0edafa2de6821884de8201b59eaae4ebf
Benjamin <benkertson@gmail.com>
2022-11-29 10:12:05 +1100

    Created sign up component, the form input component and the button component

--- src/routes/navigation/sign-in/sign-in.component.jsx
+++ src/routes/navigation/sign-in/sign-in.component.jsx
@@ -1,16 +1,38 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../../utils/firebase/firebase.utils.js";
+//import { useEffect } from 'react';
+//import { getRedirectResult } from 'firebase/auth';
+import {
+    //auth,
+    signInWithGooglePopup,
+    signInWithGoogleRedirect,
+    createUserDocumentFromAuth,
+} from "../../../utils/firebase/firebase.utils";
+import SignUpForm from "../../../components/sign-up-form/sign-up-form.component.jsx";
 
-const signIn=()=>{
-    const logGoogleUser=async()=>{
-        const { user }=await signInWithGooglePopup();
-        createUserDocumentFromAuth(user);
-    }
-    return (
-        <div>
-            <h2>Sign in to manage your orders</h2>
-            <button onClick={logGoogleUser}>Sign in with Google popups</button>
-        </div>
-    )
-}
+const SignIn=()=>{
+    /*
+    useEffect(()=>{
+        (async()=>{
+            const response=await getRedirectResult(auth);
+            if(response){
+                const userDocRef=await createUserDocumentFromAuth(response.user);
+                console.log(response);
+            }else{
+                console.log("There was no response to log");
+            };
+        })();
+    },[]);
+    */
+   const logGoogleUser=async()=>{
+    const { user }=await signInWithGooglePopup();
+    const userDocRef=await createUserDocumentFromAuth(user);
+   };
+   return (
+    <div>
+        <h2>Sign in to manage your orders and accounts</h2>
+        <button onClick={ logGoogleUser }>Sign in with Google popup</button>
+        <SignUpForm/>
+    </div>
+   )
+};
 
-export default signIn;
+export default SignIn;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,10 +1,13 @@
 import { initializeApp } from 'firebase/app';
+
 import {
     getAuth,
     signInWithRedirect,
     signInWithPopup,
     GoogleAuthProvider,
+    createUserWithEmailAndPassword,
 } from 'firebase/auth';
+
 import {
     getFirestore,
     doc,
@@ -12,20 +15,20 @@ import {
     setDoc,
 } from 'firebase/firestore';
 
-const firebaseConfig = {
+const firebaseConfig={
 
     apiKey: "AIzaSyA-xxgvpjXOSlt4VvJfSo7RgeBEyvDfEnU",
-  
+
     authDomain: "rubber-to-runway.firebaseapp.com",
-  
+
     projectId: "rubber-to-runway",
-  
-    storageBucket: "rubber-to-runway.appspot.com",
-  
+    
+    storageBucket: "rubber-to-runway.appstop.com",
+
     messagingSenderId: "345143883557",
-  
+
     appId: "1:345143883557:web:5336c0d8d812d8863acccc"
-  
+
 };
 
 const firebaseapp=initializeApp(firebaseConfig);
@@ -37,25 +40,46 @@ provider.setCustomParameters({
 });
 
 export const auth=getAuth();
+
 export const signInWithGooglePopup=()=>signInWithPopup(auth, provider);
+
+export const signInWithGoogleRedirect=()=>signInWithRedirect(auth, provider);
+
 export const db=getFirestore();
-export const createUserDocumentFromAuth=async(userAuth)=>{
-    const userDocRef=doc(db, 'users', userAuth.uid);
+
+export const createUserDocumentFromAuth=async(userAuth, additionalInformation={})=>{
+
+    if(!userAuth) return;
+
+    const userDocRef=doc(db,'users',userAuth.uid);
     const snapShot=await getDoc(userDocRef);
     console.log(snapShot.exists());
     if(!snapShot.exists()){
-        const {displayName, email}=userAuth;
+
+        const { displayName, email }=userAuth;
         const createdAt=new Date();
 
         try{
+
             await setDoc(userDocRef, {
                 displayName,
                 email,
-                createdAt
+                createdAt,
+                ...additionalInformation
             });
+
         }catch(error){
             console.log("Error creating user document", error.message);
-        }
-    }
+        };
+        
+    };
+
     return userDocRef;
+
+};
+
+export const createAuthUserWithEmailAndPassword=async(email, password)=>{
+    if(!email || !password) return;
+
+    return await createUserWithEmailAndPassword(auth, email, password);
 };
